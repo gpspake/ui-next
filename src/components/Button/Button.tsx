b43f626d786b1d31c6216fdcdd52fdefa934b189
George Spake <george@georgespake.com>
2022-05-24 21:28:17 -0500

    add styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,10 @@
-import React from "react";
+import React from "react"
+import "./Button.css"
 
 export interface ButtonProps {
-  label: string;
+  label: string
 }
 
-const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
-};
+const Button = (props: ButtonProps) => <button>{props.label}</button>
 
-export default Button;
+export default Button
